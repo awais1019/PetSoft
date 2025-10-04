ae947813171ad79deb056be53539b1ef35172ba8
Awais Ashraf <mhawais154@gmail.com>
2025-10-04 13:58:14 +0500

    change middlware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,10 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth"; 
 import { nextAuthEdgeConfig } from "./lib/auth-edge";
 
-export default NextAuth(nextAuthEdgeConfig).auth;
+export const { auth } = NextAuth(nextAuthEdgeConfig);
+
+export default auth;
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
-// export const config = {
-//   matcher: ["/app/:path*"],
-// };
